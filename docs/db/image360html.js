090ca38e9ca1e5773c26df4f81106cbea3f69922
Engelbert Niehaus <niehaus@uni-landau.de>
2019-12-03 14:09:55 +0100

    load_images2zip added

---
+++ docs/db/image360html.js
@@ -0,0 +1,20 @@
+vDataJSON.image360html = `
+<!DOCTYPE html>
+<HTML>
+	<HEAD>
+		<meta charset='utf-8'>
+		<meta repository='https://www.gitlab.com/niehausbert/aframe360' created='11.10.2019'>
+		<TITLE>360 Aframe Panorama</TITLE>
+		<meta name="description" content="360&deg; Image - A-Frame">
+		<script src='../js/aframe-v0.9.2.js'></script>
+	</HEAD>
+	<BODY>
+		<a-scene>
+			<a-assets>
+			</a-assets>
+			<a-sky src="data:___MIME_TYPE___;base64,___BASE64_IMAGE___" rotation='0 -130 0'></a-sky>
+      <!-- <a-sound src="src: url(audio/ocean_waves.mp3)" autoplay="true" position="0 2 5"></a-sound> -->
+     </a-scene>
+  </BODY>
+</HTML>
+      `;
---
+++ docs/db/index360html.js
@@ -0,0 +1,26 @@
+vDataJSON["index360html"] = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>WikiCommons2AFrame - Load Images into ZIP</title>
+    <!-- Main CSS -->
+    <link href="css/main.css" rel="stylesheet">
+  </head>
+  <body>
+    <hr>
+    <center>
+        <h2>Wiki Commons to 360-Degree List</h2>
+        <h3><i>based on AFrame Library: <a href="https://aframe.io/" target="_blank">AFrame 3D Models</a></i></h3>
+    </center>
+    <hr>
+    <p>
+      <a href="https://www.github.com/niebert/WikiCommons2AFrame" target="_blank">WikiCommons2AFrame</a> will create a 360 Degree Image on the fly and create a <a href="https://aframe.io/examples/showcase/sky/" target="_blank">AFrame VR</a>.<br>
+      The WikiCommon image as source image is used (see <a href="https://en.wikiversity.org/wiki/3D_Modelling/Create_3D_Models/Wiki_360_Degree_AFrame" target="_blank">Create 360 Degree Image for AFrame</a>)
+    </p>
+    <ul>___IMAGE360_LIST___
+    </ul>
+  </body>
+</html>
+`;
+console.log("LOG: vDataJSON.index360html='" + vDataJSON.index360html + "' in Library");
